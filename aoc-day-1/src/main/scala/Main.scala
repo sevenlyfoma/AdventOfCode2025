75ed2dce7c2a86ad64170dae2f2e4cfde3e3183f
7lyfoma <7lyfoma@gmail.com>
2025-12-01 13:49:38 +0000

    added string splitting

--- aoc-day-1/src/main/scala/Main.scala
+++ aoc-day-1/src/main/scala/Main.scala
@@ -1,5 +1,34 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+import scala.io.Source
 
-def msg = "I was compiled by Scala 3. :)"
+
+@main def main(): Unit =
+  // val lines = Source.fromFile("src/main/resources/input.txt").getLines();
+  val lines = Source.fromFile("src/main/resources/test1.txt").getLines();
+
+  val position = 50;
+  val zeroCount = 0;
+
+
+  for line <- lines do
+    //Get left or right
+    val lr = line.charAt(0);
+    //Assume nice input
+    val num = (line.substring(1)).toInt
+
+
+    if (lr == 'L')
+      ()
+    else 
+      ()
+
+    println(position)
+    println(zeroCount)
+    println()
+
+ 
+
+
+
+// L towards lower numbers R towards higher numbers 0-99 distance value 5 L10 -> 95 11 R8 -> 19
+// The dial starts at 50
+// Count the number of times the dial points at 0
