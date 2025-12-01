34126e5b82a91e5df06c7b76ef03a4b99c8fbc4f
7lyfoma <7lyfoma@gmail.com>
2025-12-01 14:46:28 +0000

    update star1 to work with multiple wraparounds, did star2 calcs for right

--- aoc-day-1/src/main/scala/Main.scala
+++ aoc-day-1/src/main/scala/Main.scala
@@ -4,6 +4,41 @@ import scala.math
 
 @main def main(): Unit =
   star_one()
+  // star_two()
+
+def star_two(): Unit = 
+  //val lines = Source.fromFile("src/main/resources/input.txt").getLines();
+  val lines = Source.fromFile("src/main/resources/test1.txt").getLines();
+
+  var position = 50;
+  var virualPos = 0;
+  var zeroCount = 0;
+  var clicksAdd = 0;
+
+  for line <- lines do
+    //Get left or right
+    var lr = line.charAt(0);
+    //Assume nice input
+    var num = (line.substring(1)).toInt
+
+
+    if (lr == 'L')
+      //need to add 100 first so that wrap around works
+      
+      position = (100 + ((position - num) % 100)) % 100
+    else 
+      virualPos = position + num;
+      clicksAdd = virualPos/100;
+      position = (position + num) % 100
+
+    if (position == 0)
+      zeroCount += clicksAdd;
+
+
+  println(zeroCount)
+
+
+  ()
 
 def star_one(): Unit = 
   val lines = Source.fromFile("src/main/resources/input.txt").getLines();
@@ -22,12 +57,18 @@ def star_one(): Unit =
 
     if (lr == 'L')
       //need to add 100 first so that wrap around works
-      position = ((100 + position) - num) % 100
+      // position = ((100 + position) - num) % 100
+
+      position = (100 + ((position - num) % 100)) % 100
     else 
       position = (position + num) % 100
 
     if (position == 0)
       zeroCount += 1;
+
+    // println(position);
+    // println(zeroCount);
+    // println();
 
 
   println(zeroCount)
