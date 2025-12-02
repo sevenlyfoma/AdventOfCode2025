8d71dd3624bdf57e7d7260c743e64f7efadc3363
7lyfoma <7lyfoma@gmail.com>
2025-12-02 10:43:34 +0000

    add input files and function for adding input files

--- aoc-day-2/src/main/scala/Main.scala
+++ aoc-day-2/src/main/scala/Main.scala
@@ -1,5 +1,24 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+import scala.io.Source
+import scala.math
+import scala.collection.mutable.ArrayBuffer
 
-def msg = "I was compiled by Scala 3. :)"
+@main def hello(): Unit =
+  loadValues("test.txt")
+
+
+def loadValues(filename: String): ArrayBuffer[(Int, Int)] =
+  var values = ArrayBuffer[(Int, Int)]()
+
+  var textInput =  Source.fromResource(filename).getLines().next()
+
+  for pair <- textInput.split(",") do
+    var split = pair.split("-")
+    var one = split(0).toInt
+    var two = split(1).toInt
+    var tup: (Int, Int) = (one, two)
+    values.addOne(tup)
+    
+
+  println(values);
+  
+  values
