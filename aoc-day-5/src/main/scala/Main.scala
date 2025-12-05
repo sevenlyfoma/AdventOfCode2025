ea347789ad67e5cceff76bf1dec134300006e97e
7lyfoma <7lyfoma@gmail.com>
2025-12-05 15:22:03 +0000

    finish code fo day 5 star 2

--- aoc-day-5/src/main/scala/Main.scala
+++ aoc-day-5/src/main/scala/Main.scala
@@ -4,6 +4,7 @@ import scala.collection.mutable.ArrayBuffer
 
 @main def main(): Unit =
   star_one()
+  star_two()
 
 def star_one(): Unit = 
   // var values = loadValues("test.txt")
@@ -13,6 +14,57 @@ def star_one(): Unit =
 
   println(count)
   ()
+
+def star_two(): Unit = 
+  // var values = loadValues("test.txt")._1
+  var values = loadValues("input.txt")._1
+
+
+  var folded = foldRangesRecursive(values)
+
+  var count = sumRanges(folded)
+
+  println(count)
+
+def sumRanges(ranges: ArrayBuffer[NumRange]) : Long =
+  var sum = 0.toLong;
+  for (r <- ranges) do
+    sum += (r.getEndR() - r.getStartR()) + 1.toLong
+  
+  return sum
+
+def foldRangesRecursive(ranges: ArrayBuffer[NumRange]): ArrayBuffer[NumRange] =
+  if (ranges.length == 1) then
+    return ranges
+  var allPartsButHead = foldRangesRecursive(ranges.tail)
+
+  var head = ranges.head;
+
+  var newRanges = ArrayBuffer[NumRange]();
+
+  for (r <- allPartsButHead) do
+    if (head.inRange(r.getStartR()) || head.inRange(r.getEndR()) || r.inRange(head.getStartR()) || r.inRange(head.getEndR())) then
+      var start = 0.toLong
+      var end = 0.toLong
+      if (head.getStartR() < r.getStartR()) then
+        start = head.getStartR()
+      else
+        start = r.getStartR()
+
+      if (head.getEndR() > r.getEndR()) then
+        end = head.getEndR()
+      else
+        end = r.getEndR()
+      head = NumRange(start, end)
+    else
+      newRanges.addOne(r)
+  
+  newRanges.addOne(head)
+
+  newRanges 
+
+
+
 
 def countInRange(ranges: ArrayBuffer[NumRange], tests: ArrayBuffer[Long]): Int =
   var count = 0
@@ -39,6 +91,19 @@ def countInRange(ranges: ArrayBuffer[NumRange], tests: ArrayBuffer[Long]): Int =
 class NumRange(startR: Long, endR: Long):
   def inRange(test: Long): Boolean =
     (test >= startR) && (test <= endR)
+
+  def getInRange(): ArrayBuffer[Long] = 
+    var ret = ArrayBuffer[Long]()
+    for (x <- startR to endR) do 
+      ret.addOne(x)
+
+    ret
+    
+  def getStartR(): Long =
+    startR
+  def getEndR(): Long =
+    endR
+
 
   override def toString(): String =
     "range(" + startR.toString() + "-" + endR.toString() + ")"
