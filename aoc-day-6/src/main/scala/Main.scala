94d98cdd0686689ef735677a766c02bd88f5a477
7lyfoma <7lyfoma@gmail.com>
2025-12-06 11:30:20 +0000

    add code for day six star one

--- aoc-day-6/src/main/scala/Main.scala
+++ aoc-day-6/src/main/scala/Main.scala
@@ -1,5 +1,81 @@
-@main def hello(): Unit =
-  println("Hello  world!".split(" +"))
-  println(msg)
+import scala.io.Source
+import scala.math
+import scala.collection.mutable.ArrayBuffer
 
-def msg = "I was compiled by Scala 3. :)"
+@main def hello(): Unit =
+  
+  star_one();
+
+def star_one(): Unit = 
+  // var values = loadValues("test.txt")
+  var values = loadValues("input.txt")
+
+  println(values)
+
+  var calced = values.map(_.doCalc())
+
+  var summed = calced.reduce((x:Long,y:Long) => x + y)
+  
+  println(summed)
+
+class Operation(values: ArrayBuffer[Long], op: String): 
+
+  def doCalc(): Long = 
+    var result: Long = 0
+    if (op == "+") then
+      result = values.reduce((x:Long,y:Long) => x + y)
+    if (op == "*") then 
+      result = values.reduce((x:Long,y:Long) => x * y)
+    result
+
+  override def toString(): String =
+    "Operation(" + values.toString() + "," + op + ")"
+
+
+def loadValuesNew(filename: String): ArrayBuffer[Operation] = 
+  var lines =  Source.fromResource(filename).getLines()
+
+  var allinputs = ArrayBuffer[ArrayBuffer[String]]();
+
+  var values = ArrayBuffer[Operation]()
+
+
+  values
+
+
+
+
+def loadValues(filename: String): ArrayBuffer[Operation] = 
+  var values = ArrayBuffer[Operation]()
+  var lines =  Source.fromResource(filename).getLines()
+
+  var allinputs = ArrayBuffer[ArrayBuffer[String]]();
+
+  for (line <- lines) do 
+    var lineBuffer = ArrayBuffer[String]()
+    var word = ""
+    var justAdded = false
+    for (x <- line) do
+      justAdded = false
+      if (x == ' ') then
+        if (!word.equals("")) then
+          lineBuffer.addOne(word)
+          word = ""
+          justAdded = true
+      else
+        word = word + x.toString()
+    if (!justAdded) then
+      lineBuffer.addOne(word)
+
+    // println(lineBuffer)
+    allinputs.addOne(lineBuffer)
+
+  for (i <- 0 until allinputs(0).length) do 
+    var vals = ArrayBuffer[Long]()
+    for (j <- 0 until allinputs.length - 1) do 
+      vals.addOne(allinputs(j)(i).toLong)
+    
+    var op = Operation(vals, allinputs(allinputs.length-1)(i))
+    values.addOne(op)
+
+  values
