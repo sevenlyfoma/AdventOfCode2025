65c1d0151a3ab2b4121ec3f2f8b4fbef543b5e26
7lyfoma <7lyfoma@gmail.com>
2025-12-09 11:43:31 +0000

    add input loading to day 9

--- aoc-day-9/src/main/scala/Main.scala
+++ aoc-day-9/src/main/scala/Main.scala
@@ -1,5 +1,20 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+import scala.io.Source
+import scala.math
+import scala.collection.mutable.ArrayBuffer
+import scala.math.pow
 
-def msg = "I was compiled by Scala 3. :)"
+@main def hello(): Unit =
+  var values = loadValues("test.txt")
+
+  println(values)
+
+
+def loadValues(filename: String): ArrayBuffer[(Long, Long)] = 
+  var values = ArrayBuffer[(Long, Long)]()
+  var lines =  Source.fromResource(filename).getLines()
+
+  for (line <- lines) do
+    var split = line.split(",")
+    values.addOne((split(0).toLong,split(1).toLong))
+
+  values
