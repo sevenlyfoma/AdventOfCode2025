0391d1a3523f8a27ebe2eb6ede9a1c8c566de9d9
7lyfoma <7lyfoma@gmail.com>
2025-12-11 14:42:39 +0000

    add data reading for day 10

--- aoc-day-10/src/main/scala/Main.scala
+++ aoc-day-10/src/main/scala/Main.scala
@@ -1,5 +1,48 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+import scala.io.Source
+import scala.math
+import scala.collection.mutable.ArrayBuffer
+import scala.math.pow
+import scala.math.abs
+import scala.util.boundary, boundary.break
 
-def msg = "I was compiled by Scala 3. :)"
+
+@main def hello(): Unit =
+  var machines = loadValues("test.txt")
+  println(machines)
+
+
+class Machine(var lights: ArrayBuffer[Int], var buttons: ArrayBuffer[ArrayBuffer[Int]], var joltages: ArrayBuffer[Int]):
+  override def toString(): String =
+    "Lights: " +lights.toString() + " Buttons: " + buttons.toString() + " Joltages: " + joltages.toString();
+
+def loadValues(filename: String): ArrayBuffer[Machine] = 
+  var values = ArrayBuffer[Machine]()
+  var lines =  Source.fromResource(filename).getLines()
+
+  for (line <- lines) do
+    var split = line.split(" ")
+    
+    var lightsString = split(0)
+
+    var buttonsStrings = split.slice(1, split.length-1)
+
+    var joltagesString = split(split.length-1)
+
+    var lights = lightsString.slice(1,lightsString.length()-1).map((x => if (x == '.') then 0 else 1)).to(ArrayBuffer)
+
+    var buttons = ArrayBuffer[ArrayBuffer[Int]]();
+
+    for (bs<- buttonsStrings) do
+      var button = bs.slice(1,bs.length()-1).split(",").map(x=> x.toInt).to(ArrayBuffer)
+      buttons.addOne(button)
+
+    var joltages = joltagesString.slice(1, joltagesString.length()-1).split(",").map(x=>x.toInt).to(ArrayBuffer)
+
+
+    var m = Machine(lights, buttons, joltages)
+
+    values.addOne(m);
+
+
+
+  values
