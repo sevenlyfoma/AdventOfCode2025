0e0a03f05370168c989991043890f8904d32fa59
7lyfoma <7lyfoma@gmail.com>
2025-12-16 13:57:28 +0000

    add non tail recursive solution for day 10 star 2

--- aoc-day-10/src/main/scala/Main.scala
+++ aoc-day-10/src/main/scala/Main.scala
@@ -7,14 +7,7 @@ import scala.util.boundary, boundary.break
 
 
 @main def hello(): Unit =
-  // var machines = loadValues("test.txt")
-  // // println(machines)
 
-  // // getAllButtonCombinationsOfSizeN(machines(0).buttons.length, 2)
-
-  // var subsets = getPowerSet(machines(0).buttons.length)
-
-  // println(findShortestSequence(machines(0), subsets))
   // star_one();
   star_two();
   ()
@@ -28,145 +21,125 @@ import scala.util.boundary, boundary.break
 
 
 def star_two(): Unit = 
-  var machines = loadValues("input.txt")
-  // var machines = loadValues("test.txt")
+  // var machines = loadValues("input.txt")
+  var machines = loadValues("test.txt")
+
+  // println(find_joltages_setup(machines(0)));
+  // println(find_joltages_setup(machines(1)));
+  // println(find_joltages_setup(machines(2)));
 
   var sum = 0;
 
-  var count = 0;
-
   for (m <- machines) do
-    sum += findShortestJoltageSequence(m)
-
-    count += 1;
-
-    println(count)
+    sum += find_joltages_setup(m)
 
   println(sum)
   
-  // for (i <- 1 until 15) do 
-  //   println(i)
-  //   println(getCombinationsOfSizeN(4, i))
 
-  // println(getCombinationsOfSizeN(4, 10))
+def find_joltages_setup(m: Machine): Int =
+  var powerSet = getPowerSet(m.buttons.length).to(ArrayBuffer);
 
-def findShortestJoltageSequence(m: Machine): Int = 
-  var shortestSequence = m.joltages.max - 1;
-  var found = false;
-  var numberOfButtons = m.buttons.length
-
-  var newLights = m.joltages.clone()
-
-  while (!found) do
-    shortestSequence += 1;
-    
-
-    var counters = ArrayBuffer[Int]();
-    for (i <-0 until shortestSequence) do
-      counters.addOne(numberOfButtons-1);
-
-    while (counters(0) != -1 && (found == false)) do
-      found = found || testJoltageSequence(m, counters, newLights);
-
-      counters(counters.length-1) -= 1
-
-      updateCountersAfterSubtractionUnit(numberOfButtons, counters.length-1, counters);
-    
-
-    // var combinations = getCombinationsOfSizeN(m.buttons.length, shortestSequence);
+  find_jolatages_recursive(m, powerSet, 1, 0);
 
 
-    // for (c <- combinations) do
+def find_jolatages_recursive(m: Machine, powerSet: ArrayBuffer[Set[Int]], multiplicationValue: Int, runningTotal: Int): Int = 
+  // println()
+  // println(m.joltages)
+  
+  if (m.joltages.reduce((x, y) => x + y) != 0) then
+    if (multiplicationValue == 1) then
+      m.changeLightsToJoltageOdds();
+      var singleSeq = findShortestSequenceAsSeq(m, powerSet.iterator)// * multiplicationValue
+
       
 
-    // if (shortestSequence > 10) then
-    //   found = true
-    //   shortestSequence = 101
-  // println(shortestSequence)
-  
-  shortestSequence
+      var m2 = Machine(m.lights.clone(), m.buttons.clone(), m.joltages.clone());
 
+      m2.changeLightsToJoltageOdds();
 
-def testJoltageSequence(m: Machine, buttonPresses: ArrayBuffer[Int], newLights: ArrayBuffer[Int]): Boolean =
-  for (i<- 0 until m.joltages.length) do
-    newLights(i) = m.joltages(i)
+      var secondSingleSeq = findSecondShortestSequenceAsSeq(m2, powerSet.iterator)
 
+      m.reduceJoltagesByButtons(singleSeq._2);
+      m.halfJoltages();
 
-  for (i <- buttonPresses) do
-    var button = m.buttons(i)
-    for (j <- button) do
-      newLights(j) -= 1;
+      m2.reduceJoltagesByButtons(secondSingleSeq._2);
+      m2.halfJoltages();
 
-  var allOff = true;
+      var x1 = find_jolatages_recursive(m, powerSet, 2, 0)
 
-  for (x <- newLights) do
-    allOff = allOff && (x <= 0)
+      var x2 = find_jolatages_recursive(m2, powerSet.clone(), 2,0)
 
-  // newLights.map(x => if (x == 0) then true else false).reduce((x,y) => x && y)
+      var t1 = singleSeq._1 + 2*x1
+      var t2 = secondSingleSeq._1 + 2*x2
 
-  allOff
-def updateCountersAfterSubtractionUnit(numberOfButtons: Int, pivot: Int, counters: ArrayBuffer[Int]): Unit = 
-  if (counters(pivot) == -1) then
-    if (pivot == 0) then
-      ()
+      // println(t1.toString() + " " + t2.toString())
+
+      if (t1<t2) then
+        t1
+      else
+        t2
     else
-      counters(pivot - 1) -= 1;
-      counters(pivot) = numberOfButtons - 1;
+      m.changeLightsToJoltageOdds();
+      // println(m.lights)
 
-      updateCountersAfterSubtractionUnit(numberOfButtons, (pivot-1), counters);
+      var singleSeq = findShortestSequenceAsSeq(m, powerSet.iterator)// * multiplicationValue
+
+      var seqSize = singleSeq._1
+      var seq = singleSeq._2
+
+    
+
+      m.reduceJoltagesByButtons(seq);
+      // println(m.joltages)
+      m.halfJoltages();
+
+      // println(m.lights)
+
+      // println(singleSeq)
+
+      
+      var x = find_jolatages_recursive(m, powerSet, 2, 0)
+
+      seqSize + 2* (x)
+
 
   else
-    ()
-
-// def updateCountersAfterSubtraction(numberOfButtons: Int, pivot: Int, counters: ArrayBuffer[Int]): ArrayBuffer[Int] =
-//   if (counters(pivot) == -1) then
-//     if (pivot == 0) then
-//       counters
-//     else
-//       counters(pivot - 1) -= 1;
-//       counters(pivot) = numberOfButtons - 1;
-
-//       updateCountersAfterSubtraction(numberOfButtons, (pivot-1), counters);
-
-//   else
-//     counters
+    0//runningTotal
 
 
-
-// def getCombinationsOfSizeN(numberOfButtons: Int, n: Int): ArrayBuffer[ArrayBuffer[Int]] = 
-//   var combinations = ArrayBuffer[ArrayBuffer[Int]]()
-
-//   var counters = ArrayBuffer[Int]();
-//   for (i <-0 until n) do
-//     counters.addOne(numberOfButtons-1);
-
-//   while (counters(0) != -1) do
-//     combinations.addOne(counters.clone())
-
-//     counters(counters.length-1) -= 1
-
-//     counters = updateCountersAfterSubtraction(numberOfButtons, counters);
+def findSecondShortestSequenceAsSeq(m: Machine, combinations: Iterator[Set[Int]]): (Int, Set[Int]) = 
+  var haveFoundOne = false
+  var minPresses = Int.MaxValue;
+  var minComb = Set[Int]()
 
 
-//   combinations = combinations.map(x => x.sorted).distinct
+  while (!haveFoundOne) do
+    if (combinations.hasNext) then
+      var combination = combinations.next();
 
-//   combinations
+      if (testSequence(m, combination)) then
+        if (combination.size <= minPresses) then
+            minPresses = combination.size
+            minComb = combination
+          
+        else
+          haveFoundOne = true
+          minPresses = combination.size
+          minComb = combination
+    else
+      haveFoundOne = true
 
-// def updateCountersAfterSubtraction(numberOfButtons: Int, counters: ArrayBuffer[Int]): ArrayBuffer[Int] =
-//   if (counters(counters.length-1) == -1) then
-//     if (counters.length == 1) then
-//       counters
-//     else
-//       var smallerCounters = counters.slice(0, counters.length-1);
-//       smallerCounters(smallerCounters.length - 1) -= 1;
+    
+    
+  (minPresses, minComb)
 
-//       var newCounters = updateCountersAfterSubtraction(numberOfButtons, smallerCounters);
-//       newCounters.addOne(numberOfButtons-1)
-//       newCounters
 
-//   else
-//     counters
+  
 
+
+
+
+  
 
 def star_one(): Unit =
   // var machines = loadValues("test.txt")
@@ -181,6 +154,24 @@ def star_one(): Unit =
 
   println(sum)
 
+
+def findShortestSequenceAsSeq(m: Machine, combinations: Iterator[Set[Int]]): (Int, Set[Int]) = 
+  var haveFoundOne = false
+  var minPresses = Int.MaxValue;
+  var minComb = Set[Int]()
+
+  while (!haveFoundOne) do
+    if (combinations.hasNext) then
+      var combination = combinations.next();
+
+      if (testSequence(m, combination)) then
+        minPresses = combination.size
+        minComb = combination
+        haveFoundOne = true
+    else
+      haveFoundOne = true
+    
+  (minPresses, minComb)
 
 
 def findShortestSequence(m: Machine, combinations: Iterator[Set[Int]]): Int = 
@@ -231,6 +222,31 @@ class Machine(var lights: ArrayBuffer[Int], var buttons: ArrayBuffer[ArrayBuffer
   override def toString(): String =
     "Lights: " +lights.toString() + " Buttons: " + buttons.toString() + " Joltages: " + joltages.toString();
 
+  def changeLightsToJoltageOdds(): Unit =
+    for (i <- 0 until lights.length) do
+      if (joltages(i) % 2 == 0) then
+        lights(i) = 0
+      else
+        lights(i) = 1
+
+  def reduceJoltagesByLights(): Unit = 
+    for (i <- 0 until lights.length) do
+      if (lights(i) != 0) then
+        joltages(i) -= 1;
+
+  def reduceJoltagesByButtons(set: Set[Int]): Unit = 
+
+    for (i <- set) do
+      var but = buttons(i)
+      for (j <- but) do
+        joltages(j) -= 1
+
+  
+  def halfJoltages(): Unit =
+    for (i <- 0 until lights.length) do
+      joltages(i) /= 2;
+
+
 def loadValues(filename: String): ArrayBuffer[Machine] = 
   var values = ArrayBuffer[Machine]()
   var lines =  Source.fromResource(filename).getLines()
@@ -262,3 +278,264 @@ def loadValues(filename: String): ArrayBuffer[Machine] =
 
 
   values
+
+
+
+// [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+// [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+// [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
+
+// import scala.io.Source
+// import scala.math
+// import scala.collection.mutable.ArrayBuffer
+// import scala.math.pow
+// import scala.math.abs
+// import scala.util.boundary, boundary.break
+
+
+// @main def hello(): Unit =
+
+//   // star_one();
+//   star_two();
+//   ()
+
+
+// //Nice solution to copy at some point
+// //https://www.reddit.com/r/adventofcode/comments/1pk87hl/comment/ntp4njq/
+
+// //Linear algebra solution to understand at some point
+// //https://www.reddit.com/r/adventofcode/comments/1pl8nsa/2025_day_10_part_2_is_this_even_possible_without/
+
+
+// def star_two(): Unit = 
+//   // var machines = loadValues("input.txt")
+//   var machines = loadValues("test.txt")
+
+//   println(find_joltages_setup(machines(0)));
+  
+
+// def find_joltages_setup(m: Machine): Int =
+//   var powerSet = getPowerSet(m.buttons.length).to(ArrayBuffer);
+
+//   find_jolatages_recursive(m, powerSet, 1, 0);
+
+
+// def find_jolatages_recursive(m: Machine, powerSet: ArrayBuffer[Set[Int]], multiplicationValue: Int, runningTotal: Int): Int = 
+  
+//   if (m.joltages.reduce((x, y) => x + y) != 0) then
+
+//     // if (multiplicationValue == 0) then
+//     //   var singleSeq = findShortestSequenceAsSeq(m, powerSet.iterator)// * multiplicationValue
+
+//     //   var secondSingleSeq = findSecondShortestSequenceAsSeq(m, powerSet.iterator)
+
+//     //   var m2 = Machine(m.lights.clone(), m.buttons.clone(), m.joltages.clone());
+
+//     //   m.reduceJoltagesByButtons(singleSeq._2);
+//     //   m.halfJoltages();
+
+//     //   m2.reduceJoltagesByButtons(secondSingleSeq._2);
+//     //   m2.halfJoltages();
+
+//     //   var x1 = find_jolatages_recursive(m, powerSet, 2, 0)
+
+//     //   var x2 = find_jolatages_recursive(m2, powerSet, 2,0)
+
+//     //   var t1 = singleSeq._1 + 2*x1
+//     //   var t2 = secondSingleSeq._1 + 2*x2
+
+//     //   if (t1<t2) then
+//     //     t1
+//     //   else
+//     //     t2
+
+//     // else
+
+
+//     var singleSeq = findShortestSequenceAsSeq(m, powerSet.iterator)// * multiplicationValue
+//     var seqSize = singleSeq._1
+//     var seq = singleSeq._2
+
+
+//     m.reduceJoltagesByButtons(seq);
+//     m.halfJoltages();
+
+    
+//     var x = find_jolatages_recursive(m, powerSet, multiplicationValue*2, runningTotal + seqSize)
+
+//     seqSize + 2* (x)
+
+
+//   else
+//     0//runningTotal 
+  
+
+
+
+
+  
+
+// def star_one(): Unit =
+//   // var machines = loadValues("test.txt")
+//   var machines = loadValues("input.txt")
+
+//   var sum = 0;
+
+//   for (m <- machines) do
+//     var combinations = getPowerSet(m.buttons.length);
+
+//     sum += findShortestSequence(m, combinations);
+
+//   println(sum)
+
+// def findSecondShortestSequenceAsSeq(m: Machine, combinations: Iterator[Set[Int]]): (Int, Set[Int]) = 
+//   var haveFoundOne = false
+//   var minPresses = Int.MaxValue;
+//   var minComb = Set[Int]()
+
+
+//   while (!haveFoundOne) do
+//     var combination = combinations.next();
+
+//     if (testSequence(m, combination)) then
+//       if (combination.size <= minPresses) then
+//           minPresses = combination.size
+//           minComb = combination
+        
+//       else
+//         haveFoundOne = true
+//         minPresses = combination.size
+//         minComb = combination
+
+    
+    
+//   (minPresses, minComb)
+
+
+// def findShortestSequenceAsSeq(m: Machine, combinations: Iterator[Set[Int]]): (Int, Set[Int]) = 
+//   var haveFoundOne = false
+//   var minPresses = Int.MaxValue;
+//   var minComb = Set[Int]()
+
+//   var highestSlotCount = 0
+
+//   while (!haveFoundOne) do
+//     var combination = combinations.next();
+
+//     if (testSequence(m, combination)) then
+//       minPresses = combination.size
+//       minComb = combination
+//       haveFoundOne = true
+        
+        
+
+
+    
+    
+//   (minPresses, minComb)
+
+
+// def findShortestSequence(m: Machine, combinations: Iterator[Set[Int]]): Int = 
+//   var haveFoundOne = false
+//   var minPresses = 0;
+
+//   while (!haveFoundOne) do
+//     var combination = combinations.next();
+
+//     if (testSequence(m, combination)) then
+//       minPresses = combination.size
+//       haveFoundOne = true
+    
+//   minPresses
+
+
+
+
+// def testSequence(m: Machine, buttonPresses: Set[Int]): Boolean =
+//   var newLights = m.lights.clone();
+
+//   for (i <- buttonPresses.iterator) do
+//     var button = m.buttons(i)
+//     for (j <- button) do
+//       var light = newLights(j)
+//       if (light == 0) then
+//         newLights(j) = 1
+//       else
+//         newLights(j) = 0
+
+//   newLights.map(x => if (x == 0) then true else false).reduce((x,y) => x && y)
+
+
+// def getPowerSet(numberOfButtons: Int): Iterator[Set[Int]] = 
+
+//   var s = Set[Int]()
+
+//   for (i <- 0 until numberOfButtons) do
+//     s = s + i;
+  
+//   s.subsets()
+    
+
+ 
+  
+
+// class Machine(var lights: ArrayBuffer[Int], var buttons: ArrayBuffer[ArrayBuffer[Int]], var joltages: ArrayBuffer[Int]):
+//   override def toString(): String =
+//     "Lights: " +lights.toString() + " Buttons: " + buttons.toString() + " Joltages: " + joltages.toString();
+
+//   def changeLightsToJoltageOdds(): Unit =
+//     for (i <- 0 until lights.length) do
+//       if (joltages(i) % 2 == 0) then
+//         lights(i) = 0
+//       else
+//         lights(i) = 1
+
+//   def reduceJoltagesByLights(): Unit = 
+//     for (i <- 0 until lights.length) do
+//       if (lights(i) != 0) then
+//         joltages(i) -= 1;
+
+//   def reduceJoltagesByButtons(set: Set[Int]): Unit = 
+
+//     for (i <- set) do
+//       var but = buttons(i)
+//       for (j <- but) do
+//         joltages(j) -= 1
+
+  
+//   def halfJoltages(): Unit =
+//     for (i <- 0 until lights.length) do
+//       joltages(i) /= 2;
+
+
+// def loadValues(filename: String): ArrayBuffer[Machine] = 
+//   var values = ArrayBuffer[Machine]()
+//   var lines =  Source.fromResource(filename).getLines()
+
+//   for (line <- lines) do
+//     var split = line.split(" ")
+    
+//     var lightsString = split(0)
+
+//     var buttonsStrings = split.slice(1, split.length-1)
+
+//     var joltagesString = split(split.length-1)
+
+//     var lights = lightsString.slice(1,lightsString.length()-1).map((x => if (x == '.') then 0 else 1)).to(ArrayBuffer)
+
+//     var buttons = ArrayBuffer[ArrayBuffer[Int]]();
+
+//     for (bs<- buttonsStrings) do
+//       var button = bs.slice(1,bs.length()-1).split(",").map(x=> x.toInt).to(ArrayBuffer)
+//       buttons.addOne(button)
+
+//     var joltages = joltagesString.slice(1, joltagesString.length()-1).split(",").map(x=>x.toInt).to(ArrayBuffer)
+
+
+//     var m = Machine(lights, buttons, joltages)
+
+//     values.addOne(m);
+
+
+
+//   values
